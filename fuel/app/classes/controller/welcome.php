7a169661a1357299d2fe4f4ff7948969e65af696
Mike Griffiths <mike@recklessnewmedia.com>
2013-01-10 14:38:12 +0000

    Initial commit

---
+++ fuel/app/classes/controller/welcome.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * The Welcome Controller.
+ *
+ * A basic controller example.  Has examples of how to set the
+ * response body and status.
+ * 
+ * @package  app
+ * @extends  Controller
+ */
+class Controller_Welcome extends Controller_Template
+{
+
+	/**
+	 * The basic welcome message
+	 * 
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_index()
+	{
+		$data = array();
+		$this->template->title = 'Example Page';
+        $this->template->content = View::forge('welcome/index', $data);		
+	}
+
+	/**
+	 * A typical "Hello, Bob!" type example.  This uses a ViewModel to
+	 * show how to use them.
+	 * 
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_hello()
+	{
+		return Response::forge(ViewModel::forge('welcome/hello'));
+	}
+
+	/**
+	 * The 404 action for the application.
+	 * 
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_404()
+	{
+		return Response::forge(ViewModel::forge('welcome/404'), 404);
+	}
+}
---
+++ fuel/app/views/welcome/index.php
@@ -0,0 +1,5 @@
+
+<h2>Get Started</h2>
+<p>The controller generating this page is found at <code>APPPATH/classes/controller/welcome.php</code>.</p>
+<p>This view can be found at <code>APPPATH/views/welcome/index.php</code>.</p>
+<p>You can modify these files to get your application started quickly.</p>
