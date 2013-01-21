4140b0f22e712bd8431ee071733808514c3fe5c8
Mike Griffiths <mike@recklessnewmedia.com>
2013-01-21 14:14:38 +0000

    Grabbing info from freebase about birthdays

--- fuel/app/modules/birthdayinfo/classes/controller/birthdayinfo.php
+++ fuel/app/modules/birthdayinfo/classes/controller/birthdayinfo.php
@@ -25,35 +25,7 @@ class Controller_Birthdayinfo extends \Controller_Template
 		$data = array();
 		$val = \Validation::forge('my_validation');
 		
-		$q = array(
-			'type' => '/people/person',
-			'limit' => 1000,
-			'sort' => 'name',
-			'name' => null,
-			'guid' => null,
-			'timestamp' => null,
-			'/people/person/date_of_birth' => '1955-02-24'
-		);
-		$query_str = json_encode($q);
-		$query = 'https://www.googleapis.com/freebase/v1/mqlread?query=['.$query_str.']';
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $query);
-		echo 'Called <a href="'.$query.'">'.$query.'</a><br>';
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		//curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_VERBOSE, 1);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt ($ch, CURLOPT_TIMEOUT, 4000);  // times out after 4s
-		curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, false); // allow redirects 
-				
-		// Execute query and get raw json results
-		$result_str = curl_exec($ch);
-		echo $result_str;
-		
-		
 		$val->add_field('birthday', 'your birthday', 'valid_string[utf8]|required');
-		$val->add_field('futuredate', 'the future date', 'valid_string[utf8]|required');
 		$val->set_message('required', 'You must specify :label in order for us to calculate your age.');
 		
 		if ($val->run())
@@ -70,30 +42,49 @@ class Controller_Birthdayinfo extends \Controller_Template
 				$ok_to_proceed = true;
 			}
 
-			// Process the date
-			$futuredate = \Input::post('futuredate');
-			$futuredate_arr = explode('/', $futuredate);
-			if(count($futuredate_arr) != 3){
-				$data['error'] = 'Sorry, the future date was entered in the wrong format.';
-				$ok_to_proceed = false;
-			} else {
-				$ok_to_proceed = true;
-			}
-
 			if($ok_to_proceed){
 				// Convert dates to timestamps
 				$birthday_timestamp = mktime(0, 0, 0, $birthday_arr[0], $birthday_arr[1], $birthday_arr[2]);
-				$futuredate_timestamp = mktime(0, 0, 0, $futuredate_arr[0], $futuredate_arr[1], $futuredate_arr[2]);
 				$data['birthday_timestamp'] = $birthday_timestamp;
-				$data['futuredate_timestamp'] = $futuredate_timestamp;
 				// Now lets calculate all the useless facts
 				$difference = array();
 				/*$difference['seconds'] = $futuredate_timestamp - $birthday_timestamp;
 				$difference['minutes'] = $difference['seconds']/60;
 				$difference['hours'] = $difference['minutes']/60;*/
 				$date1 = new \DateTime('@'.$birthday_timestamp);
-				$date2 = new \DateTime('@'.$futuredate_timestamp);
+				$date2 = new \DateTime('@'.time());
 				$data['interval'] = (array)$date1->diff($date2);
+				
+								
+				/* Grab all info on the date.  Bit useless
+				$q = array(
+					'/type/reflect/any_value' => array(
+						'type' => '/type/datetime',					
+						'value' => date('Y-m-d', $birthday_timestamp),
+						'link' => array(
+							'source' => array(
+								'id' => null,
+								'name' => null,
+								'type' => array()
+							)
+						),
+						'*' => null
+					)
+				);
+				$res = $this->freebase($q);*/
+				
+				$q = array(
+					'type' => '/people/person',
+					'limit' => 1000,
+					'sort' => 'name',
+					'name' => null,
+					'guid' => null,
+					'timestamp' => null,
+					'/people/person/date_of_birth' => date('Y-m-d', $birthday_timestamp)
+				);
+				$birthdays = $this->freebase($q);
+				
+				echo '<pre>'.print_r($birthdays, true).'</pre>';
 			}
 			
 		}
@@ -109,4 +100,27 @@ class Controller_Birthdayinfo extends \Controller_Template
         $this->template->javascript = \View::forge('js/index', $data);		
         $this->template->content = \View::forge('index', $data);		
 	}
+	
+	
+	public function freebase($q)
+	{
+		$query_str = json_encode($q);
+		$query = 'https://www.googleapis.com/freebase/v1/mqlread?query=['.$query_str.']';
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $query);
+		echo 'Called <a href="'.$query.'">'.$query.'</a><br>';
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		//curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_VERBOSE, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt ($ch, CURLOPT_TIMEOUT, 4000);  // times out after 4s
+		curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, false); // allow redirects 
+				
+		// Execute query and get raw json results
+		$result_str = curl_exec($ch);	
+		$res = json_decode($result_str);
+		return $res;
+	}
 }
+
--- fuel/app/modules/birthdayinfo/views/index.php
+++ fuel/app/modules/birthdayinfo/views/index.php
@@ -1,16 +1,9 @@
-<h1>How Old Will I be In...</h1>
+<h1>Birthday Info</h1>
 
 <p>
-	Use this tool to work out <strong>how old you will be</strong>
-  on a specific date in the future.
-</p>
-<p>
-  Just give us your birthday and the future date you want to
-  check your age on and we'll do the rest.
-</p>
-<p>
-  Don't worry - we don't save anything you put in, so we won't
-  be recording who you are or what your birthday is.
+	Find out some interesting facts about your birthday,
+    such as who shares your birthday or how many days old
+    you are.
 </p>
 
 <?php echo $val->show_errors(); ?>
@@ -23,12 +16,6 @@
       <?php echo Form::input('birthday', \Input::Post('birthday'), array('placeholder' => 'Date', 'class' => 'datepicker input-small')); ?>
     </div>
   </div>
-  <div class="control-group">
-    <label class="control-label" for="inputEmail">How old will I be in...</label>
-    <div class="controls">
-      <?php echo Form::input('futuredate', \Input::Post('futuredate'), array('placeholder' => 'Date', 'class' => 'datepicker2 input-small')); ?>
-    </div>
-  </div>
   <div class="form-actions">
     <?php echo Form::submit('submit', 'Calculate', array('class' => 'btn')); ?>
   </div>
@@ -37,7 +24,7 @@
 <a name="results"></a>
 <?php if(!empty($futuredate_timestamp)): ?>
 <div class="well">
-	<p>
+  <p>
     In <?=date('Y', $futuredate_timestamp)?> you will be <strong><?=$interval['y']?></strong> years, <strong><?=$interval['m']?></strong> months and <strong><?=$interval['d']?></strong> days old.
   </p>
 </div>
